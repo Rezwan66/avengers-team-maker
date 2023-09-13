e5637b2b6e23c47e4e527a990ac32e87feef71d3
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-14 01:17:43 +0600

    first commit- tailwind, daisy installed, with data stored as json file

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-primary-content text-primary-content rounded-full">
+        <a className="btn btn-ghost normal-case text-xl text-primary w-full rounded-full">
+          Avengers Team
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
